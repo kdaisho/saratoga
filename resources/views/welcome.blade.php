5b66e6798b3f66dfc437d93962fdf1cbf6f64d5a
Daisho <daishokomiyama@gmail.com>
2017-10-22 01:38:17 -0400

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Saratoga
                 </div>
 
                 <div class="links">
